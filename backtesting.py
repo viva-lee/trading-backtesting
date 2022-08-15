2491854656dde4d88f1aa93fd0798fb69466ac9c
viva-lee <phoenix71405@gmail.com>
2022-08-15 23:17:44 +0900

    mdd 함수

--- backtesting.py
+++ backtesting.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # 기간수익률
 def calculate_hpr(initial_value, final_value):
     hpr = (final_value - initial_value) / initial_value
@@ -9,3 +11,10 @@ def calculate_cagr(initial_value, final_value, period):
     cagr = (final_value / initial_value) ** (1 / period) - 1
     cagr = round(cagr*100, 2)
     return cagr
+
+# 최대 낙폭
+def calculate_mdd(price_array):
+    arr = np.array(price_array)
+    idx_lower = np.argmin(arr - np.maximum.accumulate(arr))
+    idx_upper = np.argmax(arr[:idx_lower])
+    return (arr[idx_lower] - arr[idx_upper]) / arr[idx_upper]
